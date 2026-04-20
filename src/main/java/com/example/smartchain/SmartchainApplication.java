2eee2ccfc773333eaf29cd53d9fd38b6c000c8da
ramakrishnalavu <srivenkatasivaramakrishna_lavu@srmap.edu.in>
2026-04-20 11:20:04 +0530

    Initial commit

---
+++ src/main/java/com/example/smartchain/SmartchainApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartchainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartchain/SmartchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
